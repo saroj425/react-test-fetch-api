ebad680d8bb0a95d50ca7b4954f8d73145209779
Sarojmilkanoagro <saroj@milkanoagro.com>
2023-12-15 06:35:21 +0530

    display data in table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import axios  from 'axios';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  const [data,setData] = useState([0]);
+  const [query, setQuery] = useState("")
+
+  const apidata = async()=>{
+    const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    console.log("",response.data);
+    setData(response.data);
+    console.log("sdfjflskfjk",data)
+  }
+
+  const searchTitle = ()=>{
+    const filtereddata = data.filter()
+  }
+
+  console.log(data)
+  useEffect(()=>{
+    apidata();
+  },[query])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <span><input type='text' placeholder='seacth data' value={query} onChange={(e)=>setQuery(e.target.value)}/> <button >Search</button></span>
+      </div>
+      <table>
+        
+        {          
+          data.map((newdata)=> (         
+              <tr>
+                <td>{newdata.userId}</td>
+                <td>{newdata.id}</td>
+                <td>{newdata.title}</td>
+                <td>{newdata.body}</td>
+              </tr>            
+          ))
+        }
+        </table>
     </div>
   );
 }
